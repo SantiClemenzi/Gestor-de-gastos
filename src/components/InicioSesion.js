0540cd7ad834e65daadebd08d09188c70878fb28
santiago clemenzi <clemenzisantiago@gmail.com>
2022-02-12 17:57:08 -0300

    iniciar-sesion_creado

--- src/components/InicioSesion.js
+++ src/components/InicioSesion.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 // paquetes
 import { Helmet } from 'react-helmet';
 // elementos
 import { Header, Titulo, ContenedorHeader } from '../elements/Header';
-import BotonRegresar from '../elements/BotonRegresar';
+import Boton from './../elements/Boton';
+import {
+	Formulario,
+	Input,
+	ContenedorBoton,
+} from './../elements/ElementosDeFormulario';
+// imagen svg
+import { ReactComponent as SvgLogIn } from './../images/login.svg';
 
 const InicioSesion = () => {
 	return (
@@ -13,12 +21,40 @@ const InicioSesion = () => {
 			</Helmet>
 			<Header>
 				<ContenedorHeader>
-					<BotonRegresar />
 					<Titulo>Iniciar Sesion</Titulo>
+					<div>
+						<Boton to="/registroUsuario">Registrarse</Boton>
+					</div>
 				</ContenedorHeader>
 			</Header>
+			<Formulario>
+				<Svg />
+				<Input
+					type="email"
+					name="email"
+					placeholder="nombre@correo.com"
+					required
+				/>
+				<Input
+					type="password"
+					name="password"
+					placeholder="Contraseña"
+					required
+				/>
+			</Formulario>
+			<ContenedorBoton>
+				<Boton as="button" type="submit" primario>
+					Iniciar Sesion
+				</Boton>
+			</ContenedorBoton>
 		</>
 	);
 };
+
+const Svg = styled(SvgLogIn)`
+	width: 100%;
+	max-height: 13rem;
+	margin-bottom: 1.25rem;
+`;
 
 export default InicioSesion;
